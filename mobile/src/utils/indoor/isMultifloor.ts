24cefad13304b28fead90263f65c74ecdde4bd1a
VladimirTodorov21 <arnolito@gmail.com>
2026-03-22 23:22:58 -0400

    Ran Lint/Prettier

--- mobile/src/utils/indoor/isMultifloor.ts
+++ mobile/src/utils/indoor/isMultifloor.ts
@@ -1,8 +1,7 @@
 export const isMultiFloor = (pathSteps: { icon: string; label: string }[]): boolean => {
-  
   for (const step of pathSteps) {
-    const hasStairs = step.label.includes('Stairs')
-    const hasElevators = step.label.includes('Elevator')
+    const hasStairs = step.label.includes('Stairs');
+    const hasElevators = step.label.includes('Elevator');
     if (hasStairs || hasElevators) {
       return true;
     }
